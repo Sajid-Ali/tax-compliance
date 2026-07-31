f4f61ce22fd23fbf03b458f73fb3f3a7e1d3d3e1
Sajid Ali <asajid451@gmail.com>
2026-07-31 19:34:05 +0300

    Sync design-system preview cache with latest components

--- .design-sync/previews/Label.tsx
+++ .design-sync/previews/Label.tsx
@@ -1,4 +1,4 @@
-import { Label } from '../../components/ui/input';
+import { Label } from "../../components/ui/input";
 
 export function Default() {
   return <Label htmlFor="company-name">Company name</Label>;
